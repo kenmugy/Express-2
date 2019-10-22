eec02930d2bcd782c84d9fca7169992cb1d23707
ken <kenmugy@gmail.com>
2019-10-22 22:21:16 +0300

    added token verification

--- app.js
+++ app.js
@@ -11,10 +11,16 @@ app.get('/api', (req, res) => {
 });
 
 // now creating app to protect
-app.post('/api/posts', (req, res) => {
-  res.json({
-    message: 'Post Created ....'
-  });
+app.post('/api/posts', verifyToken, async (req, res) => {
+  try {
+    const authData = await jwt.verify(req.token, 'hexagon');
+    res.json({
+      message: 'Post Created ....',
+      authData
+    });
+  } catch (error) {
+    res.send(error);
+  }
 });
 
 app.post('/api/login', async (req, res) => {
@@ -32,5 +38,23 @@ app.post('/api/login', async (req, res) => {
     res.json({ error });
   }
 });
+
+function verifyToken(req, res, next) {
+  // get auth header value
+  const bearerHeader = req.headers['authorization'];
+  // check if bearer is undefined
+  if (typeof bearerHeader !== 'undefined') {
+    // split at space
+    const bearer = bearerHeader.split(' ');
+    // get token from array
+    const bearerToken = bearer[1];
+    // set token
+    req.token = bearerToken;
+    next();
+  } else {
+    //   forbidden
+    res.sendStatus(403);
+  }
+}
 
 app.listen(port, () => console.log(`listening on port ${port}`));
