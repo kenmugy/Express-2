0349e9a9eb825b2de63da91e8a53b4986a421c49
ken <kenmugy@gmail.com>
2019-10-22 21:02:15 +0300

    modified get verb

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
 
 const app = express();
 const port = process.env.PORT || '1118';
 
-app.get('/', (req, res) => {
-  res.send('api is here');
+app.get('/api', (req, res) => {
+  res.send('Welcome, api is here');
 });
 
 app.listen(port, () => console.log(`listening on port ${port}`));
